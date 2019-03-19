4b821d3528179cd8bfa752d3843f7b58091a1039
Dilraj Singh <dilrajkehra@gmail.com>
2019-03-19 10:45:07 -0500

    Added a page with list of courses and their details

--- MVC-Assignment1/Controllers/HomeController.cs
+++ MVC-Assignment1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_Assignment1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,13 @@ namespace MVC_Assignment1.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext DbContext;
+
+        public HomeController()
+        {
+            DbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +33,27 @@ namespace MVC_Assignment1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Courses()
+        {
+            List<Models.Classes.Course> courses =
+                (from course in DbContext.Courses
+                 select course).ToList();
+
+            return View(courses);
+        }
+
+        public ActionResult CourseDetails(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction(nameof(HomeController.Courses));
+            }
+
+            var course = DbContext.Courses.FirstOrDefault(p => p.Id == id);
+
+            return View(course);
         }
     }
 }
